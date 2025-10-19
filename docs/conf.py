94ab2ac2236d605f2ad3646d9dc3735fd70feeb4
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-10-19 06:11:39 -0300

    fix: Remove heading reference to ubuntu.com/ai (#59)

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ html_context = {
     # TODO: If there's no such website,
     #       remove the {{ product_page }} link from the page header template
     #       (usually .sphinx/_templates/header.html; also, see README.rst).
-    "product_page": "ubuntu.com/ai",
+    # "product_page": "ubuntu.com/ai", ## leaving commented out due to: https://github.com/canonical/inference-snaps/issues/57
     # Product tag image; the orange part of your logo, shown in the page header
     #
     # TODO: To add a tag image, uncomment and update as needed.
